b9ebc6cea454192a3b934f4eecc77eccd59b9324
Terry Rosen <tcrosen@gmail.com>
2012-03-16 17:08:17 -0400

    added files for the first time

---
+++ js/site.js
@@ -0,0 +1,21 @@
+var cities = [
+		{ID: 1, Name: 'Toronto'},
+		{ID: 2, Name: 'Montreal'},
+		{ID: 3, Name: 'New York'},
+		{ID: 4, Name: 'Buffalo'},
+		{ID: 5, Name: 'Boston'},
+		{ID: 6, Name: 'Columbus'},
+		{ID: 7, Name: 'Dallas'},
+		{ID: 8, Name: 'Vancouver'},
+		{ID: 9, Name: 'Seattle'},
+		{ID: 10, Name: 'Los Angeles'}
+	];
+
+$(function(){
+	$('#typeaheadTest').typeahead({
+		source: cities,
+		matchProp: 'Name',
+		sortProp: 'Name',
+		valueProp: 'ID'
+	});
+});
