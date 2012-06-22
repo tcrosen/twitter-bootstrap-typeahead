8b25bfa78d18ddebf7645619c94f1988d91c388f
Terry Rosen <tcrosen@gmail.com>
2012-06-22 10:40:50 -0400

    ajax support added

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -55,4 +55,23 @@ $(function () {
         display: 'full_name',
         itemSelected: displayResult
     });
+
+
+    $.mockjax({
+        url: '/cities/list',
+        responseText: [{ id: 1, name: 'Toronto' },
+				    { id: 2, name: 'Montreal' },
+				    { id: 3, name: 'New York' },
+				    { id: 4, name: 'Buffalo' },
+				    { id: 5, name: 'Boston' },
+				    { id: 6, name: 'Columbus' },
+				    { id: 7, name: 'Dallas' },
+				    { id: 8, name: 'Vancouver' },
+				    { id: 9, name: 'Seattle' },
+				    { id: 10, name: 'Los Angeles' }]
+    });
+
+    $('#demo4').typeahead({
+        ajax: '/cities/list'
+    });
 });
